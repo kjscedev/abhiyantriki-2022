0e2cae4482efbafe778774ad90c2c03283a27b7a
Vatsal <bevatsal1122@gmail.com>
2022-11-16 23:29:47 +0530

    Setup codebase     Co-authored-by: Arya Nair <aryaajitnair@gmail.com>     Co-authored-by: Pranay <hailhydra1@users.noreply.github.com>     Co-authored-by: Vinay Kanse <VinayKanase@users.noreply.github.com>

---
+++ src/data/eventsData.ts
@@ -0,0 +1,234 @@
+export const eventData = [
+  
+  {
+    linkHash: "technical",
+    eventNamePath: process.env.PUBLIC_URL + "/assets/gifs/technical.gif",
+    eventName: "Technical",
+    events: [
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/crakathon.png",
+        name: "Crackathon",
+        text: "",
+        memberLimit: "3 members per team",
+        prize: "worth 13,000/-",
+        fees: "100Rs Per Team",
+        contactInfo: ["Tanvi: +91 82918 85532"," Jay: +91 97305 35624"," Soumen: +91 83568 53262"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/codeclan.png",
+        name: "Codespree",
+        text: "Prepare to be dazzled by an adrenaline inducing Abhiyantriki pre-event CodeSpree. A national level competitive programming competition to held on CodeChef in all the mainstream languages",
+        prize: "worth 12,000/-",
+        memberLimit:"3 members per team",
+        fees: "Free",
+        contactInfo: ["Tanvi: +91 82918 85532","Jay: +91 97305 35624"," Soumen: +91 83568 53262"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/the rising star.png",
+        name: "Bid-By-Bit",
+        text: "Are you focusing on developing your finance dexterity along with coding as your past-time delectation, then you should consider registering for this one-of-a-kind event. CodeClan is a game that combines technical as well as financial-management skills with enticing rewards on the line for participants.",
+        memberLimit: "3 members per team",
+        prize: "worth 10,000/-",
+        fees: "100Rs Per Team",
+        contactInfo: ["Kanishk: +91 83568 76018", "Varshaah: +91 83693 88790"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/technocrats.png",
+        name: "Electrohunt",
+        text: "",
+        memberLimit: "3 members per team",
+        prize: "worth 6,000/-",
+        fees: "100Rs",
+        contactInfo: ["Sanyukta: +91 90822 65841", " Megh: +91 93217 72945"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/circuitsaga.png",
+        name: "Peer Pong",
+        text: "",
+        // memberLimit: "1-3 members per team",
+        prize: "worth 4,000/-",
+        fees: "30Rs per Participant",
+        contactInfo: ["Gaurav Mehta: +91 7045186349", " Vallari Kulkarni: +91 7718939746"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/electronic wizard.png",
+        name: "Operation Graduation",
+        text: "",
+        // memberLimit: "1 or 2 members",
+        prize: "worth 6,000/-",
+        fees: "100Rs per team",
+        contactInfo: ["Aarya Rane: +91 91378 13676", " Srikar Bommireddypalli : +91 99697 22298"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/incognito probe.png",
+        name: "The Bro Code",
+        text: "",
+        memberLimit: "2-4 members per team",
+        prize: "worth 7,000/-",
+        fees:"Individual Rs60\nTeam of 2:Rs80",
+        contactInfo: ["Khushi: +91 89897 40769", "Ranak: +91 99200 87619"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/tech-events/the rising star.png",
+        name: "Rush",
+        text: "",
+        memberLimit: "2 members per team",
+        prize: "worth 4,000/-",
+        fees: "100Rs Per Team",
+        contactInfo: ["Gia: +91 98205 34426", " Rishul: +91 70218 94566"],
+      },
+      
+    ],
+  },
+  {
+    linkHash: "ideate",
+    eventNamePath: process.env.PUBLIC_URL + "/assets/gifs/ideate.gif",
+    eventName: "Ideate",
+    events: [
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/ideate/nif.png",
+        name: "UNL",
+        text: "",
+        memberLimit: "3 members per team",
+        prize: "Prize: 3 months, full-time internship in Delhi for the acclaimed MNC, UNL",
+        fees: "FREE",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/ideate/mpcb.png",
+        name: "NRDC",
+        text: "Problem Statement Cost efficient & Eco-friendly technologies or solutions to encounter threat to marine and coastal ecosystem by Oil Spills.",
+        memberLimit: "3 members per team",
+        // prize: "Free internship at NIF",
+        fees: "FREE",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+
+      },
+    ],
+  },
+  {
+    linkHash: "shield",
+    eventNamePath: process.env.PUBLIC_URL + "/assets/gifs/shield.gif",
+    eventName: "Shield",
+    events: [
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/shield/tech hunt.png",
+        name: "Tech Hunt",
+        text: "A classic game of follow the clues but with a element of surprise, where your technical knowledge will be pushed to its limits. So sign right up for this event that is sure to test both your bookish knowledge as well as street smarts! Happy hunting!",
+        memberLimit: "4 members per team",
+        prize: "-",
+        fees: "-",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/shield/tech quiz.png",
+        name: "Tech Quiz",
+        text: "A questionnaire to test your technical knowledge and rack your brains to their optimum potential.This is a golden opportunity to test your knowledge and your ability to think and act on your feet! We look forward to having all of you experience the same!",memberLimit: "3 members per team",
+        prize: "-",
+        fees: "-",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+        
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/shield/technovate.png",
+        name: "Technovate",
+        text: "Participants are expected to formulate engineering solutions to the underlying issues  hindering the smooth functioning of various aspects of the society.",
+        memberLimit: "2 members per team",
+        prize: "-",
+        fees: "-",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/shield/espionage.png",
+        name: "Tech Debate",
+        text: "A debate is an organized argument or contest of ideas in which the participants discuss a  topic from two opposing sides. Those who agree with this statement or idea are the 'Government'  side. Those who will not agree with this statement or idea are the Opposition side.",
+        memberLimit: "2 members per team",
+        prize: "-",
+        fees: "-",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+        
+      },
+      {
+        eventImgPath:
+          process.env.PUBLIC_URL + "/assets/images/shield/tpp.png",
+        name: "Technical Paper Presentation",
+        text: "         Participants involved in this should construct a well drafted ‘Technical Paper’ on a topic  known by branch representatives. The IEEE standard format will be followed for this  competition.",
+        memberLimit: "2 members per team",
+        prize: "-",
+        fees: "-",
+        contactInfo: ["Rahi: +971 56 350 3392"],
+        
+      },
+    ],
+  },
+  // {
+  //   linkHash: "informals",
+  //   eventNamePath: process.env.PUBLIC_URL + "/assets/gifs/informals.gif",
+  //   eventName: "Informals",
+  //   events: [
+  //     {
+  //       eventImgPath:
+  //         process.env.PUBLIC_URL + "/assets/images/fun/bgmi.png",
+  //       name: "BGMI",
+  //       text: "India’s favourite mobile game is back with more intense gameplay and better features. Assemble your squad of 4 players, dive into the zone and survive the game with weapons, medics and the best teamwork you can put into! Sign up for BGMI and get a chance to cook chicken dinner and win exciting prizes.",
+  //       memberLimit: "4 members per team",
+  //       prize: "worth 14,000/-",
+  //       fees: "180 Rs for a team of 4",
+  //       contactInfo: ["Shubham: 7021081949", "Dhruv: 9987105109"],
+  //     },
+  //     {
+  //       eventImgPath:
+  //         process.env.PUBLIC_URL + "/assets/images/fun/valo.png",
+  //       name: "Valorant",
+  //       text: "Nothing’s more enjoyable than applying your wits in putting a great combination of firepower and special abilities against your opponents who will counter them with their own arsenal. Presenting to you Valorant, an FPS based PC game where you and your friends team up and ravage the map with the Radianite power.",
+  //       memberLimit: "4 members per team",
+  //       prize: "worth 14,000/-",
+  //       fees: "180 Rs for a team of 4",
+  //       contactInfo: ["Shubham: 7021081949", "Dhruv: 9987105109"],
+  //     },
+  //     {
+  //       eventImgPath:
+  //         process.env.PUBLIC_URL + "/assets/images/fun/codm.png",
+  //       name: "COD:Mobile",
+  //       text: "Bringing you the ultimate 5v5 team deathmatch battle in the form of a mobile phone, Call of Duty: Mobile will definitely bring an atmosphere of adrenaline rush. So get hold of your rifle, SMGs, shotguns, a handful of grenades and the Operator Skills and step on the battlefield.",
+  //       memberLimit: "4 members per team",
+  //       prize: "worth 14,000/-",
+  //       fees: "180 Rs for a team of 4",
+  //       contactInfo: ["Shubham: 7021081949", "Dhruv: 9987105109"],
+  //     },
+  //   ],
+  // },
+  // {
+  //   linkHash: "preevents",
+  //   eventNamePath: process.env.PUBLIC_URL + "/assets/gifs/pre-event.gif",
+  //   eventName: "Pre-events",
+  //   events: [
+  //     {
+  //       eventImgPath:
+  //         process.env.PUBLIC_URL + "/assets/images/tech-events/codespree.png",
+  //       name: "Codespree",
+  //       text: "CodeSpree is a precursor to Crackathon, which will take place during Abhiyantriki. It is a lengthy coding competition that puts your overall programming skills to the test, hosted on CodeChef. The CodeSpree winners will receive cash prizes as well as a direct entry into the third round of Crackathon!",
+  //       memberLimit: "3 members per team",
+  //       prize: "worth 8000/-",
+  //       fees: "FREE",
+  //       contactInfo: ["Rahil: 9920828960", "Soumen: 8356853262"],
+  //     },
+  //   ],
+  // },
+];
---
+++ src/data/exposAndSpeakers.tsx
@@ -0,0 +1,231 @@
+export const exposAndSpeakersDataJson = [
+  {
+    linkHash: "tech",
+    linkName: "Tech Expo",
+    gifName: "tech-expo.gif",
+    heading: "Tech Expo",
+    paraText: `Tech Expo is a unique exhibition and the most anticipated event at Abhiyantriki. With innovative technologies from all over the country, the Tech Expo contains exhibits from well-known organisations namely DRDO, ISRO, IMD, DAE and innovations ranging from speed-defying drones to humanoid robots with intellect on par with a human being. `,
+    buttonHref: "http://bit.ly/Abhiyantriki_2022_Registration",
+    buttonText: "Register",
+    cards: [
+      {
+        heading: "Indro 5.0",
+        text: "Meet the anticipated humanoid robot 'Indro 5.0', equipped with a day night vision camera with motion sensors. Created by Santosh Hulawale, this robot can maneuver on flat surface indoors and outdoors. It can also pick objects just like a human would. Get a chance to meet and interact with Indro 5.0, in person, at the Tech Expo during Abhiyantriki 2022.",
+        image: `${process.env.PUBLIC_URL}/assets/expos/tech/AVISHKAR_HYPERLOOP.png`,
+        cardHref: "#",
+      },
+      {
+        heading: "Indian Army",
+        text: "We, the Abhiyantriki 2022 organising team, are incredibly honoured and humbled to be working with the one and only The Indian Army this year. At the tech expo this year, The Indian Army will mainly be showcasing their infantry unit with weapons such as assault rifles, automatic guns, bombers, missiles, rocket launchers and much more! Seeing these weapons up close and in person will surely leave you awestruck and speechless.",
+        image: `${process.env.PUBLIC_URL}/assets/expos/tech/RIVERRECYCLE OY.png`,
+        cardHref: "#",
+      },
+      {
+        heading: "BDDS",
+        text: "The Bomb Detection and Disposal Squad has honored us with their presence in Abhiyantrik 2022! These brave men and women train meticulously and put it all on the line to diffuse and safely dispose of the explosive devices that threaten us.",
+        image: `${process.env.PUBLIC_URL}/assets/expos/tech/SAGAR DEFENCE.png`,
+        cardHref: "#",
+      },
+      // {
+      //   heading: "Hiroshi Ishiguro",
+      //   text: "Hiroshi Ishiguro, a roboticist at Japan's Osaka University, has built numerous robots. Ishiguro's focus in robot development is on creating a robot that is as close to a living human being as possible. One is the Geminoid, an android version of a middle-aged family man like himself. Ishiguro built his mechanical doppelganger out of silicone rubber, pneumatic actuators, powerful electronics, and hair from his scalp, and it performs realistic human functions such as blinking, breathing, and fidgeting with his hands. The Geminoid has even been used as a 'teacher' in his classes at Japan's Osaka University.",
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/HIROSHI ISHIGURO.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Puli Space",
+      //   text: "The Hungarian company develops a low-cost, lightweight planetary rover platform with unique mobility capabilities for carrying payloads and payload instruments capable of surviving the harsh lunar environment, exploring lunar resources, and supporting In-Situ Resource Utilization (ISRU) on the Moon. They hope to be the first Hungarian payload to land on the Moon, where they will search for water ice, explore the lunar surface, and take images and videos of the landing site.",
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/PULI SPACE.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Furhat Robotics",
+      //   text: `The Swedish robotics development company specialises in advancements in social robot construction. Furhat's mission is to use social and conversational robotics to enable a revolution in human-computer interaction. They created a social robot with limitless possibilities for research and development due to its expressibility and customizability. Its ingenious back-projection system allows a digital face to be displayed on a physical mask. Expressions and movements that mimic human behaviour, as well as vocal gestures such as "ehh", "ahh", "uhm", coughs, laughs, yawns, etc.`,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/FURHAT_ROBOTICS.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Turing Laboratory",
+      //   text: `Turing Laboratory is part of a new generation of research and development laboratories that have recently been established at Galileo University to promote the advancement of science and technology. It specialises in faster product development and providing a more secure overview of consumer needs. Leonardo GreenMoov, a telepresence robot, is one of their recent projects. It is designed to allow you and others to remotely control a humanoid robot while giving you the sensation of being in the location where the robot is.`,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/TURING LABORATORY.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "City Transformers",
+      //   text: `Introducing the world's first all-electric vehicle, hailing from Israel, that challenges mobility issues with smart solutions to city parking issues. A strikingly convenient, electrified, foldable mobility solution designed from the ground up for an unrivalled city riding experience. It is aimed at parking superpowers, superior manoeuvrability, and thoughtful productivity, and is designed around what city dwellers truly require. City Transformer was named one of the "top 10 cars of IAA mobility 2021" and one of Time magazine's "best inventions of 2020."`,
+
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/CITY_TRANSFORMER.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "GO ROLLOE",
+      //   text: `Introducing a new innovation in the field of pollution reduction in which the energy produced by spinning a vehicle's wheels drives the mechanism for cleaning the surrounding air. All the way from the UK, ROLLOE is an ingenious way to reduce air contaminants from your vehicle. It is a device that attaches to the wheels of a bicycle to filter air pollution by simply utilising the rider's motion and energy.`,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/GO ROLLOE.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Embention",
+      //   text: `Embention, the Spanish leaders in manned eVTOL flights, presented high-performance automated UAVs and eVTOL drones. They are the world's drone trendsetters, known for their Veronte Drone components. Embention collaborates with a wide range of UAVs to deliver ready-to-fly professional drones equipped with Veronte systems. These drones can be customised for surveillance, mapping, spraying, and a variety of other applications.`,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/EMBENTION.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Stargazing India",
+      //   text: `Stargazing India organises various outreach ventures to expose the wonder of outer space sightseeing to many enthusiasts throughout India. Mr. Narendra Gor began the journey in 1991. Since 1991, the Kutch Amateur Astronomers Club, which later became Stargazing India, has organised a series of sky observation events at various locations throughout Kachchh. This has piqued the interest of many people, including the Honorable Prime Minister, Shri Narendra Modi. Stargazing India even hosts special workshops and seminars for non-governmental organisations.
+      //   `,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/STAR GAZING INDIA.png`,
+      //   cardHref: "#",
+      // },
+      // {
+      //   heading: "Hexapod",
+      //   text: `With the advent of biped robots, IIT Madras' iBot Club created their own Hexapod, a six-legged autonomous robot. They have also designed it to cover any type of terrain using the machine learning paradigm's reinforcement learning. It can walk on any surface on its own, like an insect, learning as it goes.`,
+      //   image: `${process.env.PUBLIC_URL}/assets/expos/tech/HEXAPOD.png`,
+      //   cardHref: "#",
+      // },
+    ],
+  },
+  {
+    linkHash: "internship",
+    linkName: "Internship Expo",
+    gifName: "internship-expo.gif",
+    heading: "Internship Expo",
+    paraText: `Internship expo is one of the major events at Abhiyantriki. The main
+              objective of this expo is to provide students with a diverse range
+              of internship opportunities. These internships help the students
+              acquaint with a lot of new skills and give them the required work
+              experience. Internships play an essential role in magnifying the
+              morale of the students.`,
+    buttonHref: "http://bit.ly/Abhiyantriki_2022_Registration",
+    buttonText: "Register",
+    cards: [
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/biltrax.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/Embedos.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/foggers.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/Hammiters.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/kanan.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/Medpiper.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/scalling.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/Strictly_Electric.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/UCpro.png`,
+      },
+      {
+        heading: "",
+        text: "",
+        cardHref: "",
+        image: `${process.env.PUBLIC_URL}/assets/expos/internship/voila.png`,
+      },
+    ],
+  },
+  {
+    linkHash: "speaker",
+    linkName: "Speaker Session",
+    gifName: "speaker-session.gif",
+    heading: "Speaker Session",
+    paraText: `The Speaker Sessions held during Abhiyantriki are bound to leave one in awe and inspired. It provides a platform for many great personalities to share their thoughts and views with the audience about their experiences in their fields, be it technical, financial, management, administration etc. Eminent figures including Dr A P J Abdul Kalam, Dr Raghuram Rajan, His Holiness The Dalai Lama, Zakir Hussain, Dr Jayant Narlikar and many others have honoured us at Abhiyantriki.`,
+    buttonHref: "http://bit.ly/Abhiyantriki_2022_Registration",
+    buttonText: "Register",
+    cards: [
+      {
+        heading: "Sandeep Jain",
+        text: "The CEO of the most revered and widely acclaimed platform “GeeksforGeeks” grace our event as the Speaker for ABhiyantriki 2022. 'GeeksforGeeks' is a vital part of every programmers life. The vision for this platform has been to provide equal opportunites for every geek.",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Speaker/Mr AS Kiran Kumar.png`,
+      },
+      {
+        heading: "Kalpit Veerwal",
+        text: "In 2017, He became the first and only person to ever score full marks in the prestugious Joint Entrance Examination. IN 2018, He founded Acadboost He holds a B.Tech degree in Computer Science & Engineering from IIT Bombay",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Speaker/Dr. Swamy.png`,
+      },
+    ],
+  },
+  {
+    linkHash: "panel",
+    linkName: "Panel Session",
+    gifName: "panel-session.gif",
+    heading: "Panel Session",
+    paraText: `A live session with connoisseurs from myriad backgrounds,
+              the Speaker Session at Abhiyantriki is sure to leave you wanting
+              for more! Abhiyantriki has previously hosted notable speakers like
+              the Late Dr A.P.J Abdul Kalam, Dr Raghuram Rajan, the former RBI Governor,
+              His Holiness the Dalai Lama, and many more!`,
+    buttonHref: "http://bit.ly/Abhiyantriki_2022_Registration",
+    buttonText: "Register",
+    cards: [
+      {
+        heading: "Dhananjay Bendre",
+        text: "The Business Head of Orcale in India Mr. Bendre also has 11 years of experience as an incubator in one of the firsts startup incubation firms in India Orcale is the Institutional Giant in Software and Technology of the world. ",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Panel/Sathvik_Vishvanath.png`,
+      },
+      {
+        heading: "Rushabh Mehta",
+        text: "CEO and Co-founder of Frappe. He is a dynamic individual and is passionate about Open Source and ERPnext forums. His years of experience snd knowledge in the field of software and business will prove fundamental and insightful for all the future engineers of KJSCE. We sre eagerly looking forward to this discussion.",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Panel/Sidharth_Sogani.png`,
+      },
+      {
+        heading: "Gautaam Borah",
+        text: "Currently leading an e-commerce startup. He is also a globally accomplished author. His first look Monetising Innovation caters to and guide us through the questions of today’s world. He has also been the Vice President and Head of Vodafone.",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Panel/Ritesh_Bhatia.png`,
+      },
+      {
+        heading: "Deepika Singh",
+        text: "Ms. Deepika Singh is a passionate and dedicated Ex Fellow of Maharashtra Cyber Police Department. She hones skilled expertise in fields of the most significant areas, web intelligence, digital and deep web.",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Panel/Prassana_lohar.png`,
+      },
+      {
+        heading: "Venkataraman Prabhu",
+        text: "Mr Venkataraman Prabhu, the IT and Digital Strategy leader at Jio plaforms. Mr. Prabhu boasts over 20 years of experience spanning IT Strategy, product management and lateral thinking. He also specializes in Coporate Strategy, business excellence and IT consultation.",
+        image:
+        `${process.env.PUBLIC_URL}/assets/images/Panel/Prassana_lohar.png`,
+      },
+    ],
+  },
+];
